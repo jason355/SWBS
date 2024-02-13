95df58c329410013bb5f426db123368fab8ff6d7
jason355 <83809033+jason355@users.noreply.github.com>
2024-02-13 17:41:26 +0800

    update v2.9

---
+++ Linebot/v2.9/errorWeb.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, render_template
+from flask import jsonify
+from datetime import datetime
+app = Flask(__name__)
+
+# 存储错误信息的列表
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
